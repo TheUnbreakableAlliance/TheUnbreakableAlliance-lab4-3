eafc6f164940253236791adc0aa446c14cac20ad
wengwenchao123 <61003827+wengwenchao123@users.noreply.github.com>
2022-06-05 15:47:42 +0800

    Add files via upload

--- CLI_test.py
+++ CLI_test.py
@@ -19,7 +19,9 @@ class TestMutableList(unittest.TestCase):
             self.assertEqual(res, os.getcwd() + '\\' + s[1])
 
     def test_help(self):
-        lst = ['usage', 'sub-commands', 'position-arguments', 'named-arguments']
+        lst = ['usage', 'sub-commands',
+               'position-arguments',
+               'named-arguments']
         self.assertEqual(Help(lst[0]), 'usage: \n [--version | -V |-v ] '
                                        '[-h] [PATH] [-r][cat a|n|d] [-hex]')
         self.assertEqual(Help(lst[1]), 'sub-commands: \n cat a: add '
@@ -27,9 +29,11 @@ class TestMutableList(unittest.TestCase):
                                        '\n cat n: create a new file'
                                        '\n cat d: download files')
         self.assertEqual(Help(lst[2]), 'position-arguments: '
-                                       '\n PATH : Print the absolute path of the file')
+                                       '\n PATH : Print the absolute path '
+                                       'of the file')
         self.assertEqual(Help(lst[3]), 'named-arguments: '
-                                       '\n --version : Show the current command-line interface '
+                                       '\n --version : Show the current '
+                                       'command-line interface '
                                        'version(also -v -V) '
                                        '\n -h: show this help method and exit '
                                        '\n -r: Read the contents in the file '
