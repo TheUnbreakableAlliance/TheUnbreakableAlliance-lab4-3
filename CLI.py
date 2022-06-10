31358af97e943d40776adde025e2a3ec53dca98c
wengwenchao123 <61003827+wengwenchao123@users.noreply.github.com>
2022-06-10 16:49:08 +0800

    Add files via upload

--- CLI.py
+++ CLI.py
@@ -1,84 +1,113 @@
+# Author: Fan Yuxin, Weng Wenchao
 import sys
 import os
 from functools import wraps
-from typing import List, Callable, Dict
+
+'''
+Function description: Output version information
+:param s: Command entered
+Usage example: python *.py --version | -V | -v
+output:
+3.6.6 |Anaconda, Inc.| (default, Jun 5 2022, 15:38:43)
+[MSC v.1900 64 bit (AMD64)]
+'''
 
 
 def v():
-    """
-    Function description: Output version information
-    """
-    ret = sys.version
-    print(ret)
-    return ret
-
-
-def PATH(s: str) -> str:
-    """
-    Function description: Output the path of the specified file
-    :param s: Command entered
-    """
-    path = os.getcwd()
-    ret = path + '\\' + s
-    print(ret)
-    return ret
-
-
-def Help(s: str) -> str:
-    """
-    Function description: Output help information
-    :param s: Command entered
-    """
-    if s in ['usage',
-             'sub_commands',
-             'position_arguments',
-             'named_arguments']:
-        result = s + ': '
-        if s == 'usage':
-            result = result + '\n [--version | -V |-v ] ' \
-                              '[-h] [PATH] ' \
-                              '[-r][cat a|n|d] ' \
-                              '[-hex]'
-        elif s == 'sub_commands':
-            result = result + '\n cat a: add data to the file' \
-                              ' \n cat n: create a new file\n' \
-                              ' cat d: delete files'
-        elif s == 'position_arguments':
-            result = result + '\n PATH : Print the ' \
-                              'absolute path of the file'
-        elif s == 'named_arguments':
-            result = result + '\n --version : Show the current ' \
-                              'command_line interface version(also -v -V) \n' \
-                              ' -h: show this help method and exit \n' \
-                              ' -r: Read the contents in the file \n' \
-                              ' -hex: Change the input value ' \
-                              'to hex and save it to the hex.txt'
-    else:
-        result = 'no help topic match ' + s
-    print(result)
+    result = sys.version
     return result
 
 
-def read(s: str) -> List[str]:
-    """
-    Function description: Read the contents in the file
-    :param s: Command entered
-    """
-    with open(s) as f:
+'''
+Function description: Output the path of the specified file
+:param s: Command entered
+Usage example: python *.py PATH 1.txt
+output:
+E:\\lab3\\1.txt
+'''
+
+
+def PATH(s):
+    result = os.getcwd() + '\\' + s
+    return result
+
+
+'''
+Function description: Output help information
+e.g:
+usage: [--version | -V |-v ] [-h] [PATH] [-r][cat a|n|d] [-hex]
+sub-commands:
+                cat a: add data to the file
+                cat n: create a new file
+                cat d: download files
+position-arguments:
+                PATH : Print the absolute path of the file
+named-arguments:
+                --version : Show the current command-line interface version
+                -h: show this help method and exit
+                -r: Read the contents in the file
+                -hex: Change the input value to hex and save it to the hex.txt
+:param s: Command entered
+Usage example: python *.py -h usage
+output:
+usage: [--version | -V |-v ] [-h] [PATH] [-r][cat a|n|d] [-hex]
+'''
+
+
+def Help(s):
+    if s in ['usage', 'sub-commands', 'position-arguments', 'named-arguments']:
+        result = s + ': '
+        if s == 'usage':
+            result = result + '\n [--version | -V |-v ]' \
+                              ' [-h] [PATH] [-r][cat a|n|d] [-hex]'
+        elif s == 'sub-commands':
+            result = result + '\n cat a: add data to the file ' \
+                              '\n cat n: create a new file' \
+                              '\n cat d: download files'
+        elif s == 'position-arguments':
+            result = result + '\n PATH : Print the absolute path of the file'
+        elif s == 'named-arguments':
+            result = result + '\n --version : Show the current ' \
+                              'command-line interface version(also -v -V) \n' \
+                              ' -h: show this help method and exit ' \
+                              '\n -r: Read the contents in the file \n ' \
+                              '-hex: Change the input value to hex ' \
+                              'and save it to the hex.txt'
+    else:
+        result = 'no help topic match ' + s
+    return result
+
+
+'''
+Function description: Read the contents in the file
+:param s: Command entered
+Usage example: python *.py -r 1.txt
+output:
+This is 1.txt
+This file is created at 2022/6/5 10:39 in Beijing time.
+1234
+'''
+
+
+def read(s):
+    with open(s, 'r') as f:
         lines = f.readlines()
-        for i in lines:
-            print(i, end='')
-        return lines
+    return lines
 
 
-def cat(s: List) -> None:
-    """
-    Function description:
-            cat a: add data to the file
-            cat n: create a new file
-            cat d: Download files via http
-    :param s: Command entered
-    """
+'''
+Function description:
+    cat a: add data to the file
+    cat n: create a new file
+    cat d: delete a file
+:param s: Command entered
+cat a Usage example: python *.py cat a 1.txt 'the text you want to add'
+cat n Usage example: python *.py cat n new.txt
+cat d Usage example: python *.py cat d name.txt
+'''
+
+
+def cat(s):
     while len(s) > 0 and s[0] in ['n', 'a', 'd']:
         if s[0] == 'n':
             s.pop(0)
@@ -97,170 +126,157 @@ def cat(s: List) -> None:
             s.pop(0)
 
 
-def hex(s: str) -> str:
-    """
-    Function description: Change the input
-    value to hex and save it to the hex.txt
-    :param s: Command entered
-    """
-    ret = ""
-    hex1 = ['0', '1', '2', '3', '4', '5', '6',
-            '7', '8', '9', 'a', 'b', 'c', 'd',
-            'e', 'f']
+'''
+Function description: Change the input value to hex and save it to the hex.txt
+:param s: Command entered
+Usage example: python *.py  10
+output:
+Write A to hex.txt
+'''
+
+
+def hex(s):
+    result = ""
+    HEX = ['0', '1', '2', '3', '4', '5', '6', '7',
+           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
     num = int(s)
     while num > 15:
-        ret = hex1[num % 16] + ret
         num = num // 16
-    ret = hex1[num] + ret
+    result = HEX[num] + result
     with open('hex.txt', 'a') as f:
-        f.write(ret + "\n")
-    print(ret)
-    return ret
+        f.write(result + "\n")
+    return result
 
 
-class CLIDecorator(object):
-    def __init__(self, name: str) -> None:
-        print(name)
-        self.fun = {'-v': v,
-                    '-V': v,
-                    '--version': v,
-                    'PATH': PATH,
-                    '-h': Help,
-                    '-r': read,
-                    'cat': cat,
-                    '-hex': hex}
-        self.subcommand_name = []  # type: List
-        self.Coms = []  # type: List
-        self.option_command = {}  # type: Dict
+option_command = {}
+Coms = []
+fun = {'-v': v, '-V': v, '--version': v, 'PATH': PATH,
+       '-h': Help, '-r': read, 'cat': cat, '-hex': hex}
+subcommand_name = []
 
-    def command(self, f: Callable) -> Callable:
-        """
-        Used to decorate a function so that
-        the function serves as a command line interface
-        :param f: Callable
-        :return: Callable
-        """
 
-        def r(*args, **kwargs):
-            self.Coms = sys.argv[1:]
-            return f(*args, **kwargs)
+# Used to decorate a function so that the
+# function serves as a command line interface
+def command(f):
+    def r(*args, **kwargs):
+        global Coms
+        Coms = sys.argv[1:]
+        return f(*args, **kwargs)
+    return r
 
-        return r
 
-    def option(self,
-               func_name: str,
-               default=None) -> Callable:
-        """
-        Used to decorate a function
-        :param func_name: str
-        :param default: Union[str, None]
-        :return: Callable
-        """
+# Used to decorate a function
+def option(func_name, default=None):
+    def logging_decorator(func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            global option_command
+            option_command[fun[func_name]] = default
+            return func(*args, **kwargs)
+        return wrapped_function
+    return logging_decorator
 
-        def logging_decorator(func):
-            @wraps(func)
-            def wrapped_function(*args, **kwargs):
-                self.option_command[self.fun[func_name]] = default
-                return func(*args, **kwargs)
 
-            return wrapped_function
+# Pass a simple variable value
+def argument(func_name, default=None):
+    def logging_decorator(func):
+        @wraps(func)
+        def wrapped_function(*args):
+            global option_command
+            argu = default
+            subcommand_name.append(func_name)
+            fun[func_name] = func_name
+            option_command[fun[func_name]] = args
+            Coms = sys.argv[1:]
+            if func_name in Coms:
+                index = Coms.index(func_name)
+                argu = Coms[index+1]
+            func(argu)
+        return wrapped_function
+    return logging_decorator
 
-        return logging_decorator
 
-    def argument(self,
-                 func_name: str,
-                 default=None) -> Callable:
-        """
-        Pass a simple variable value
-        :param func_name: str
-        :param default: Union[str, None]
-        :return: Callable
-        """
-
-        def logging_decorator(func):
-            @wraps(func)
-            def wrapped_function(*args):
-                argu = default
-                self.subcommand_name.append(func_name)
-                self.fun[func_name] = func_name
-                self.option_command[self.fun[func_name]] = args
-                self.Coms = sys.argv[1:]
-                if func_name in self.Coms:
-                    index = self.Coms.index(func_name)
-                    if len(self.Coms) == 1:
-                        argu = None
+def run():
+    global Coms
+    while len(Coms) > 0:
+        if fun[Coms[0]] not in option_command:
+            print('\'' + Coms[0] + '\' is not availiable')
+            raise KeyError
+        elif fun[Coms[0]] is v:
+            v()
+            Coms = Coms[1:]
+        elif fun[Coms[0]] is Help:
+            if len(Coms) == 1 or Coms[1] in fun:
+                print('Invalid instruction ')
+            else:
+                c = []
+                Coms.pop(0)
+                while Coms[0] in ['usage',
+                                  'sub-commands',
+                                  'position-arguments',
+                                  'named-arguments']:
+                    c.append(Coms[0])
+                    Coms.pop(0)
+                    c.append(Coms[0])
+                    Coms.pop(0)
+                    if len(Coms) == 0:
+                        break
+                Help(c)
+        elif fun[Coms[0]] in subcommand_name:
+            break
+        elif fun[Coms[0]] is PATH:
+            if len(Coms) == 1:
+                PATH(option_command[PATH])
+                break
+            elif Coms[1] in fun:
+                PATH(option_command[PATH])
+                Coms = Coms[1:]
+            else:
+                PATH(Coms[1])
+                Coms = Coms[2:]
+        elif fun[Coms[0]] is read:
+            if len(Coms) == 1:
+                read(option_command[read])
+                break
+            elif Coms[1] in fun:
+                read(option_command[read])
+                Coms = Coms[1:]
+            else:
+                read(Coms[1])
+                Coms = Coms[2:]
+        elif fun[Coms[0]] is hex:
+            if len(Coms) == 1:
+                hex(option_command[hex])
+                break
+            elif Coms[1] in fun:
+                hex(option_command[hex])
+                Coms = Coms[1:]
+            else:
+                hex(Coms[1])
+                Coms = Coms[2:]
+        elif fun[Coms[0]] is cat:
+            if len(Coms) == 1 or Coms[1] in fun:
+                print('Invalid instruction ')
+            else:
+                c = []
+                Coms.pop(0)
+                while Coms[0] in ['n', 'a', 'd']:
+                    if Coms[0] == 'a':
+                        c.append(Coms[0])
+                        Coms.pop(0)
+                        c.append(Coms[0])
+                        Coms.pop(0)
+                        c.append(Coms[0])
+                        Coms.pop(0)
                     else:
-                        argu = self.Coms[index + 1]
-                func(argu)
+                        c.append(Coms[0])
+                        Coms.pop(0)
+                        c.append(Coms[0])
+                        Coms.pop(0)
+                    if len(Coms) == 0:
+                        break
+                cat(c)
 
-            return wrapped_function
 
-        return logging_decorator
-
-    def run(self) -> None:
-        while len(self.Coms) > 0:
-            if self.fun[self.Coms[0]] not in self.option_command:
-                print('\'' + self.Coms[0] + '\' is not availiable')
-                raise KeyError
-                break
-            elif self.fun[self.Coms[0]] is v:
-                # print(self.Coms)
-                v()
-                self.Coms = self.Coms[1:]
-            elif self.fun[self.Coms[0]] is Help:
-                Help(self.Coms[1])
-                self.Coms = self.Coms[2:]
-            elif self.fun[self.Coms[0]] in self.subcommand_name:
-                break
-            elif self.fun[self.Coms[0]] is PATH:
-                if len(self.Coms) == 1:
-                    PATH(self.option_command[PATH])
-                    break
-                elif self.Coms[1] in self.fun:
-                    PATH(self.option_command[PATH])
-                    self.Coms = self.Coms[1:]
-                else:
-                    PATH(self.Coms[1])
-                    self.Coms = self.Coms[2:]
-            elif self.fun[self.Coms[0]] is read:
-                if len(self.Coms) == 1:
-                    read(self.option_command[read])
-                    break
-                elif self.Coms[1] in self.fun:
-                    read(self.option_command[read])
-                    self.Coms = self.Coms[1:]
-                else:
-                    read(self.Coms[1])
-                    self.Coms = self.Coms[2:]
-            elif self.fun[self.Coms[0]] is hex:
-                if len(self.Coms) == 1:
-                    hex(self.option_command[hex])
-                    break
-                elif self.Coms[1] in self.fun:
-                    hex(self.option_command[hex])
-                    self.Coms = self.Coms[1:]
-                else:
-                    hex(self.Coms[1])
-                    self.Coms = self.Coms[2:]
-            elif self.fun[self.Coms[0]] is cat:
-                if len(self.Coms) == 1 or self.Coms[1] in self.fun:
-                    print('Invalid instruction ')
-                else:
-                    c = []
-                    self.Coms.pop(0)
-                    while self.Coms[0] in ['n', 'a', 'd']:
-                        if self.Coms[0] == 'a':
-                            c.append(self.Coms[0])
-                            self.Coms.pop(0)
-                            c.append(self.Coms[0])
-                            self.Coms.pop(0)
-                            c.append(self.Coms[0])
-                            self.Coms.pop(0)
-                        else:
-                            c.append(self.Coms[0])
-                            self.Coms.pop(0)
-                            c.append(self.Coms[0])
-                            self.Coms.pop(0)
-                        if len(self.Coms) == 0:
-                            break
-                    cat(c)
+if __name__ == '__main__':
+    Help('usage')
